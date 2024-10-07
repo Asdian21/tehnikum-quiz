403f298168a6176420d798de1ba8613e46e7791e
Asdian <asdiank7@gmail.com>
2024-10-07 17:29:07 +0500

    create in App.jsx a massive with objects. Each object include two keys: path and components

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,43 @@ import Welcome from "./pages/Welcome";
 import "./styles/main.css";
 import StepOne from "./pages/StepOne";
 import StepTwo from "./pages/StepTwo";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import StepFour from "./pages/StepFour";
+import StepThree from "./pages/StepThree";
+import Thanks from "./pages/Thanks";
+
+const routerConfig = createBrowserRouter([
+  {
+    path: "/",
+    components: <Welcome />,
+  },
+  {
+    path: "/step-one",
+    components: <StepOne />,
+  },
+  {
+    path: "/step-two",
+    components: <StepTwo />,
+  },
+  {
+    path: "/step-three",
+    components: <StepThree />,
+  },
+  {
+    path: "/step-four",
+    components: <StepFour />,
+  },
+  {
+    path: "/thanks",
+    components: <Thanks />,
+  },
+]);
 
 const App = () => {
   return (
     <div className="App">
-      {/* <Welcome /> */}
-      <StepOne />
+      <Welcome />
+      {/* <StepOne /> */}
       {/* <StepTwo /> */}
     </div>
   );
