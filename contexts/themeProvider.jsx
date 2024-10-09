df40516bb699e67c1819ec7d4b3ab51f4cac1907
Asdian <asdiank7@gmail.com>
2024-10-09 18:17:38 +0500

    add logic to change theme and save it to a localStorage. we use function setThemeValue.

--- contexts/themeProvider.jsx
+++ contexts/themeProvider.jsx
@@ -1,8 +1,18 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { ThemeContext } from "./themeContext";
+import { ThemeContext, themes } from "./themeContext";
 
 export const ThemeProvider = ({ children }) => {
   cosnt[(themeValue, setThemeValue)] = useState(themes.light);
+
+  // Переключение темы
+  const toggleTheme = () => {
+    setThemeValue((prev) => {
+      let newTheme = prev === themes.light ? themes.dark : theme.light;
+      localStorage.setItem("currentTheme", JSON.stringify(newTheme));
+      return newTheme;
+    });
+    // setThemeValue(themeValue === themes.dark ? themes.light : themes.dark);
+  };
 
   return <ThemeContext.Provider>{children}</ThemeContext.Provider>;
 };
