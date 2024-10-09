4e3752590976ba9cad2e4ca4a8c0b7545075a85a
Asdian <asdiank7@gmail.com>
2024-10-09 17:59:45 +0500

    create themeProvider with props = children and include ThemeContext.Provider with children

---
+++ contexts/themeProvider.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { ThemeContext } from "./themeContext";
+
+export const ThemeProvider = ({ children }) => {
+  <ThemeContext.Provider>{children}</ThemeContext.Provider>;
+};
