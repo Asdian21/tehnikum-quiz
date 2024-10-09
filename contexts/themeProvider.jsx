19a35626ca2acd7a078bca3109cbfbecbbba6141
Asdian <asdiank7@gmail.com>
2024-10-09 18:03:42 +0500

    add hooks useEffect and useMemo to use them in future. add hook useState called themeValue with star state themes.light

--- contexts/themeProvider.jsx
+++ contexts/themeProvider.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { ThemeContext } from "./themeContext";
 
 export const ThemeProvider = ({ children }) => {
-  <ThemeContext.Provider>{children}</ThemeContext.Provider>;
+  cosnt[(themeValue, setThemeValue)] = useState(themes.light);
+
+  return <ThemeContext.Provider>{children}</ThemeContext.Provider>;
 };
