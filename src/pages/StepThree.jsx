b5c19c38d6d9aa5a1471e4cfbd37d73c996e4161
Asdian <asdiank7@gmail.com>
2024-10-14 14:14:01 +0500

    add some comments to future work and add path to useState, AnswerLabel. deleted comments that I don't use

--- src/pages/StepThree.jsx
+++ src/pages/StepThree.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { LinkButton } from "../components/LinkButton";
 import { ProgressBar } from "../components/ProgressBar";
 import { Header } from "../components/Header";
+import { AnswerLabel } from "../components/AnswerLabel"; // Проверь, существует ли этот компонент
 
 const StepThree = () => {
   const [emoji, setEmoji] = useState("");
@@ -10,18 +11,22 @@ const StepThree = () => {
     {
       emoji_id: "var-1",
       emojiName: "laugh",
+      //<img src="./img/laugh.png" alt="laugh" />
     },
     {
       emoji_id: "var-2",
       emojiName: "love",
+      // <img src="./img/hearts.png" alt="hearts" />
     },
     {
       emoji_id: "var-3",
       emojiName: "cool",
+      //<img src="./img/smirk.png" alt="smirk" />
     },
     {
       emoji_id: "var-4",
       emojiName: "shock",
+      //<img src="./img/fright.png" alt="fright" />
     },
   ];
 
@@ -30,59 +35,17 @@ const StepThree = () => {
       <div className="wrapper">
         <div className="emoji-quiz">
           <ProgressBar currentStep={3} currentValue="75%" />
-          {/* <div className="indicator">
-            <div className="indicator__text">
-              <span className="indicator__description">
-                Скидка за прохождение опроса:
-              </span>
-              <span className="indicator__value">15%</span>
-            </div>
-            <div className="indicator__progressbar">
-              <div className="indicator__unit indicator__unit-1 _active"></div>
-              <div className="indicator__unit indicator__unit-2 _active"></div>
-              <div className="indicator__unit indicator__unit-3"></div>
-              <div className="indicator__unit indicator__unit-4"></div>
-            </div>
-          </div> */}
           <div className="question">
             <Header headerType="h2" headerText="Занимательный вопрос" />
             <ul className="emoji-variants">
               {emojiVariants.map((elem) => (
                 <AnswerLabel
                   id={elem.emoji_id}
-                  AnswerLabel={elem.emojiName}
+                  labelText={elem.emojiName} // Исправлено имя пропса
                   key={elem.emoji_id}
                   labelChange={() => setEmoji(elem.emojiName)}
                 />
               ))}
-              {/* <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-1" />
-                <label htmlFor="variant-1">
-                  <img src="./img/laugh.png" alt="laugh" />
-                  <p>Ваш ответ 1</p>
-                </label>
-              </li>
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-2" />
-                <label htmlFor="variant-2">
-                  <img src="./img/hearts.png" alt="hearts" />
-                  <p>Ваш ответ 2</p>
-                </label>
-              </li>
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-3" />
-                <label htmlFor="variant-3">
-                  <img src="./img/smirk.png" alt="smirk" />
-                  <p>Ваш ответ 3</p>
-                </label>
-              </li>
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-4" />
-                <label htmlFor="variant-4">
-                  <img src="./img/fright.png" alt="fright" />
-                  <p>Ваш ответ 4</p>
-                </label>
-              </li> */}
             </ul>
             <LinkButton path="/step-four" />
           </div>
