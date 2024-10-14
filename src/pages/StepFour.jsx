d8a17bd7da527609d816bb076ede2470679df5aa
Asdian <asdiank7@gmail.com>
2024-10-14 14:23:49 +0500

    delete commets about ProgressBar from StepFour

--- src/pages/StepFour.jsx
+++ src/pages/StepFour.jsx
@@ -33,20 +33,6 @@ const StepFour = () => {
       <div className="wrapper">
         <div className="emoji-quiz">
           <ProgressBar currentStep={4} currentValue="100%" />
-          {/* <div className="indicator">
-            <div className="indicator__text">
-              <span className="indicator__description">
-                Скидка за прохождение опроса:
-              </span>
-              <span className="indicator__value">15%</span>
-            </div>
-            <div className="indicator__progressbar">
-              <div className="indicator__unit indicator__unit-1 _active"></div>
-              <div className="indicator__unit indicator__unit-2 _active"></div>
-              <div className="indicator__unit indicator__unit-3 _active"></div>
-              <div className="indicator__unit indicator__unit-4"></div>
-            </div>
-          </div> */}
           <div className="question">
             <Header headerType="h2" headerText="4. Занимательный вопрос" />
             <ul className="level-variants">
