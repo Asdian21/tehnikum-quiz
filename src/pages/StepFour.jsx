de5639d8502a145f924392f76088af167d2b0494
Asdian <asdiank7@gmail.com>
2024-10-19 01:00:35 +0500

    pages StepFour and Thanks are done

--- src/pages/StepFour.jsx
+++ src/pages/StepFour.jsx
@@ -4,10 +4,52 @@ import { Header } from "../components/Header";
 import { LinkButton } from "../components/LinkButton";
 import { ThemeContext } from "../new_contexts/ThemeContext";
 import { AnswerLabel } from "../components/AnswerLabel";
+import { AppButton } from "../components/AppButton";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const StepFour = () => {
+  const location = useLocation();
+  const {
+    previousNameValue = "",
+    previousPhoneValue = "",
+    eshkereValue = "",
+    course = "",
+    emoji = "",
+  } = location.state || {};
+
   const { theme } = useContext(ThemeContext);
   const [number, setNumber] = useState("");
+  const [checkButton, setCheckButton] = useState(true);
+
+  const navigate = useNavigate();
+  const goToNextPage = () => {
+    navigate("/thanks", {
+      state: {
+        previousNameValue,
+        previousPhoneValue,
+        eshkereValue,
+        course,
+        emoji,
+        number,
+      },
+    });
+  };
+
+  useEffect(() => {
+    if (number) {
+      setCheckButton(false);
+    } else {
+      setCheckButton(true);
+    }
+  }, [number]);
+
+  const handleClick = () => {
+    if (number) {
+      goToNextPage();
+    }
+  };
 
   const levelVariants = [
     {
@@ -34,44 +76,29 @@ const StepFour = () => {
 
   return (
     <div className={`container ${theme === "dark" ? "_dark" : ""}`}>
-      <div className="container">
-        <div className="wrapper">
-          <div className="emoji-quiz">
-            <ProgressBar currentStep={4} currentValue="100%" />
-            <div className="question">
-              <Header headerType="h2" headerText="4. Занимательный вопрос" />
-              <ul className="level-variants">
-                {levelVariants.map((elem) => (
-                  <AnswerLabel
-                    id={elem.id}
-                    AnswerLabel={elem.levelId}
-                    key={elem.id}
-                    labelChange={() => setNumber(elem.levelId)}
-                  />
-                ))}
-                <li className="variant-wrapper">
-                  <input required type="radio" name="variant" id="variant-1" />
-                  <label htmlFor="variant-1">1</label>
-                </li>
-                <li className="variant-wrapper">
-                  <input required type="radio" name="variant" id="variant-2" />
-                  <label htmlFor="variant-2">2</label>
-                </li>
-                <li className="variant-wrapper">
-                  <input required type="radio" name="variant" id="variant-3" />
-                  <label htmlFor="variant-3">3</label>
-                </li>
-                <li className="variant-wrapper">
-                  <input required type="radio" name="variant" id="variant-4" />
-                  <label htmlFor="variant-4">4</label>
-                </li>
-                <li className="variant-wrapper">
-                  <input required type="radio" name="variant" id="variant-5" />
-                  <label htmlFor="variant-5">5</label>
-                </li>
-              </ul>
-              <LinkButton path="/thanks" headerText="Далее" />
-            </div>
+      <div className="wrapper">
+        <div className="emoji-quiz">
+          <ProgressBar currentStep={4} currentValue="100%" />
+          <div className="question">
+            <Header
+              headerType="h2"
+              headerText="4. Сколько часов в день вы программируете?"
+            />
+            <ul className="level-variants">
+              {levelVariants.map((elem) => (
+                <AnswerLabel
+                  id={elem.id}
+                  AnswerLabel={elem.levelId}
+                  key={elem.id}
+                  labelChange={() => setNumber(elem.levelId)}
+                />
+              ))}
+            </ul>
+            <AppButton
+              buttonClick={handleClick}
+              isDisabled={checkButton}
+              buttonText="Далее"
+            />
           </div>
         </div>
       </div>
--- src/pages/Thanks.jsx
+++ src/pages/Thanks.jsx
@@ -1,10 +1,31 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../new_contexts/ThemeContext";
 import { useLocation } from "react-router-dom";
+import { Header } from "../components/Header";
+import { AppButton } from "../components/AppButton";
 
 const Thanks = () => {
   const { theme } = useContext(ThemeContext);
   const location = useLocation();
+  const {
+    previousNameValue = "",
+    previousPhoneValue = "",
+    eshkereValue = "",
+    course = "",
+    emoji = "",
+    number = "",
+  } = location.state || {};
+
+  let emojiState = "";
+  if (emoji === "laugh") {
+    emojiState = <img src="./img/laugh.png" alt="laugh" />;
+  } else if (emoji === "love") {
+    emojiState = <img src="./img/hearts.png" alt="hearts" />;
+  } else if (emoji === "cool") {
+    emojiState = <img src="./img/smirk.png" alt="smirk" />;
+  } else if (emoji === "shock") {
+    emojiState = <img src="./img/fright.png" alt="fright" />;
+  }
 
   return (
     <div className={`container ${theme === "dark" ? "_dark" : ""}`}>
@@ -12,11 +33,20 @@ const Thanks = () => {
         <div className="wrapper">
           <div className="thanks">
             <img src="./img/bell.png" alt="bell" />
-            <h1>Спасибо за прохождение опроса!</h1>
-            <p>Получи свою скидку по ссылке ниже или другое блаблабла</p>
-            <button type="button" id="get-link">
-              Получить ссылку
-            </button>
+            <Header
+              headerText="Спасибо за прохождение опроса!"
+              headerType="h1"
+            />
+            <span>Ваше имя: {previousNameValue}</span> <br />
+            <span>Ваш номер телефона: {previousPhoneValue}</span> <br />
+            <span>
+              Вы ответили {eshkereValue} получению специального IT образования
+            </span>
+            <br />
+            <span>Выбранный вами курс: {course}</span> <br />
+            <span>Вы программируете по {number} часов в день</span> <br />
+            <span>Ваше эмоциональное состояние: {emojiState}</span>
+            <AppButton buttonType="submit" buttonText="Получить ссылку" />
           </div>
         </div>
       </div>
